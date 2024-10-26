a815b720ecc69fccb82ed248387445d99ce370af
Jidran Khan <marofijidran@gmail.com>
2024-10-27 00:56:51 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,14 @@ import footer from "./Footer/page"
 const  page = () => {
   return (
     <div>
+      <footer />
       <ul>
         <li><a href='./'>Home</a></li>
         <li><a href='./about'>About</a></li>
         <li><a href='./contact'>Contact</a></li>
         <li><a href='./services'>Services</a></li>
       </ul>
+
     </div>
      
 
